9e9b636346f7c7110335d5ff466b64f57818bf30
Alex Vergara <55516909+SentinalMax@users.noreply.github.com>
2022-03-09 21:40:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,50 @@
+import time
+import random
+import sys
 from chatterbot import ChatBot
 #from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
-# Create object of ChatBot class with Logic Adapter
+#variables
+words = " "
+
+#typing function
+def faketype(words):
+  words
+  for char in words:
+    time.sleep(random.choice([0.3, 0.11, 0.08, 0.07,   0.07, 0.07, 0.06, 0.06, 0.05, 0.01]))
+    sys.stdout.write(char)
+    sys.stdout.flush()
+  time.sleep(1)
+
+#Create object of ChatBot class with Logic Adapter
 bot = ChatBot(
     'XANDER',  
     logic_adapters=[
-       
-        'chatterbot.logic.MathematicalEvaluation',
-        'chatterbot.logic.TimeLogicAdapter',
-        'chatterbot.logic.BestMatch',]
-    )
+        'chatterbot.logic.BestMatch',
+      ],
+)
 
 #trainer = ListTrainer(bot)
 trainer = ChatterBotCorpusTrainer(bot)
 
-name=input("Enter Your Name: ")
-
-trainer.train([
+trainer.train(
     "chatterbot.corpus.english"
-])
+)
+
+name=input("Enter Your Name: ")
 
 print("Hello, " + name + ". I am XANDER; say 'bye' to exit.")
 while True:
-    request=input(name+':')
+    request=input('\n'+name+':\n')
 
     if request=='Bye' or request =='bye':
         print('XANDER: Goodbye')
         break
     else:
+        print('XANDER: ')
         response=bot.get_response(request)
-        print('Zander:',response)
+        string = str(response) #concat responses
+        faketype(string)
+        #print('XANDER:',response)
+
